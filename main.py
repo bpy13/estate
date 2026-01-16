2c3a7b0b3af95e286ba84ff19df33b483fcdeb26
bpy13 <briandyu@gmail.com>
2026-01-16 13:39:25 +0800

    quick update

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+
+# initialize data in json
 
 options = Options()
 driver = webdriver.Chrome(options=options)
+
+# loop over estates
+
 url = "https://hk.centanet.com/estate/%E5%A4%AA%E5%8F%A4%E5%9F%8E/3-OVDUURFSRJ"
 driver.get(url)
-time.sleep(3)  # Wait for page to load
-elements = driver.find_elements(By.CLASS_NAME, "nhistory_xAxis")
-if elements:
-    elements[0].click()
-    print("Clicked first element!")
-else:
-    print("No elements found!")
 
+# loop over group, then loop over buildings
+
+elements = driver.find_elements(By.CLASS_NAME, "nhistory_xAxis")
+driver.execute_script("arguments[0].click();", elements[0])
+
+# loop over flats
+
+popup = driver.find_elements(By.CLASS_NAME, "floorInfoBox")
+popup[0].text # get info
 
 driver.quit()
